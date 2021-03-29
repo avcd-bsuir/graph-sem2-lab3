e80fcbcdd7feedf9df964fc19454557eb24ed722
AlexeyFilich <alexey.filich@mail.ru>
2021-03-29 13:08:12 +0300

    Another line draw function implementation

--- include/Color.hpp
+++ include/Color.hpp
@@ -6,7 +6,6 @@
 struct Color {
     uint8_t b, g, r, a;
     Color() : r(0), g(0), b(0), a(255) {}
-    // Color(uint32_t c) : r((c & 0x00FF0000) >> 16), g((c & 0x0000FF00) >> 8), b((c & 0x000000FF)), a((c & 0xFF000000) >> 24) {}
     Color(uint32_t c) : r((c & 0x00FF0000) >> 16), g((c & 0x0000FF00) >> 8), b((c & 0x000000FF)), a(255) {}
     Color(uint8_t r, uint8_t g, uint8_t b) : r(r), g(g), b(b), a(255) {}
     Color(uint8_t r, uint8_t g, uint8_t b, uint8_t a) : r(r), g(g), b(b), a(a) {}
